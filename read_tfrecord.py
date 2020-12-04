e7aa930a1c1e9eb0b66f4c70b144250981f23e75
Jinyang Liu <jinyang.liu@beboplogin3.lcrc.anl.gov>
2020-12-04 03:41:21 -0600

    1

--- read_tfrecord.py
+++ read_tfrecord.py
@@ -1,14 +1,22 @@
 import tensorflow as tf
 
-def _parse_(example_proto,tftype):
-    #tftype: tf.float32, tf.float64
+def _parse_64(example_proto):
     features={"data":tf.FixedLenFeature((),tf.string)}
     parsed_features=tf.parse_single_example(example_proto,features)
+    data=tf.decode_raw(parsed_features['data'],tf.float64)
+    return data
+
+def _parse_32(example_proto):
+    features={"data":tf.FixedLenFeature((),tf.string)}
     data=tf.decode_raw(parsed_features['data'],tf.float32)
     return data
 
-def load_tfrecords(srcfile):
+def load_tfrecords_64(srcfile):
 #srcfile: input tfrecord file
     sess=tf.Session()
     dataset=tf.data.TFRecordDataset(srcfile)
-    dataset=dataset.map(_parse_)
+    dataset=dataset.map(_parse_64)
+def load_tfrecords_32(srcfile):
+    sess=tf.Session()
+    dataset=tf.data.TFRecordDataset(srcfile)
+    dataset=dataset.map(_parse_32)
